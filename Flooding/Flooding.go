8f095f608f112aadabec45d45fbc19b505ee0bdd
Gabriel Vaz <gabriel.vaz22@gmail.com>
2021-11-01 19:29:24 -0300

    second commit

--- Flooding/Flooding.go
+++ Flooding/Flooding.go
@@ -1,29 +1,40 @@
 package Flooding
 
-import(
+import (
 	"fmt"
 	// "os"
 	"math/rand"
 	"time"
+
 	// "log"
 	"encoding/json"
 	"strings"
+
 	. "../BEB"
 )
 
+const (
+	Prop    = "proposal"
+	Deliver = "deliver"
+	Decided = "decided"
+)
+
 type Proposal struct {
-	From    string `json:"from"`
-	Number  int `json:"number"`
+	From   string `json:"from"`
+	Number int    `json:"number"`
+	Round  int    `json:"round"`
 }
 
 type Flooding_Module struct {
-	Correct_events [] string
-	Round int
-	Decision bool
-	ReceivedFrom [][]string
-	Proposals []Proposal
-	BEB BestEffortBroadcast_Module
+	Correct_events []string
+	Round          int
+	Decision       bool
+	ReceivedFrom   [][]string
+	Proposals      [][]Proposal
+	BEB            BestEffortBroadcast_Module
 }
+
+type JSON map[string]interface{}
 
 func (module Flooding_Module) Init(addresses []string) {
 	module.Correct_events = addresses
@@ -31,7 +42,7 @@ func (module Flooding_Module) Init(addresses []string) {
 	module.Round = 1
 	module.Decision = false
 	module.ReceivedFrom = make([][]string, n_proccess)
-	// module.Proposals = make([]Proposal, n_proccess)
+	module.Proposals = make([][]Proposal, n_proccess)
 	module.ReceivedFrom[0] = module.Correct_events
 
 	module.BEB = BestEffortBroadcast_Module{
@@ -39,57 +50,125 @@ func (module Flooding_Module) Init(addresses []string) {
 		Ind: make(chan BestEffortBroadcast_Ind_Message)}
 
 	module.BEB.Init(addresses[0])
-
-	module.Send_Proposal(Proposal{From: addresses[0], Number: generateRandom()})
-	module.Receive_Proposal()
+	module.Send(Prop)
+	module.Receive()
 }
 
-func (module Flooding_Module) Send_Proposal(v Proposal){
-	print("ovo envia")
-	go func(){
-		module.Proposals = append(module.Proposals, v)
-		
-		req := BestEffortBroadcast_Req_Message{
-			Addresses: module.Correct_events[1:],
-			Message:   encodeJson(module.Proposals) + "§" + module.Correct_events[0]}
-		module.BEB.Req <- req
-	}()
-	
+func (module Flooding_Module) Send(typeMessage string) {
+	switch typeM := typeMessage; typeM {
+	case Prop:
+		prop := Proposal{From: module.Correct_events[0], Number: generateRandom(), Round: module.Round}
+		module.Proposal(prop)
+	default:
+		fmt.Printf("Outra opcao")
+	}
 }
 
-func (module Flooding_Module) Receive_Proposal(){
+func (module Flooding_Module) Receive() {
 	print("ovo recebe")
-	go func(){
+	go func() {
 		for {
 			in := <-module.BEB.Ind
 			message := strings.Split(in.Message, "§")
 			in.From = message[1]
-			// registro = append(registro, in.Message)
 			in.Message = message[0]
 
-			fmt.Printf("Message from %v: %v\n", in.From, in.Message)
-			fmt.Println(decodeJson(in.Message))
+			var data JSON
+			json.Unmarshal([]byte(in.Message), &data)
+
+			switch typeM := data["type"]; typeM {
+			case Prop:
+				// prop := Proposal{From: data[""]}
+				// prop := Proposal{From: module.Correct_events[0], Number: generateRandom(), Round: module.Round}
+				// module.Proposal(prop)
+				fmt.Printf("Prop")
+
+			default:
+				fmt.Printf("Outra opcao")
+			}
+
+			// var anyJson map[string]interface{}
+			// json.Unmarshal([]byte, &anyJson)
+			// fmt.Printf("Message from %v: %v\n", in.From, in.Message)
+
+			// receivedFrom := module.ReceivedFrom[proposal.Round]
+			// receivedFrom = append(receivedFrom, proposal.From)
+			// module.ReceivedFrom[proposal.Round] = receivedFrom
+
+			// proposals := module.Proposals[proposal.Round]
+			// proposals = append(proposals, proposal)
+			// module.Proposa	ls[proposal.Round] = proposals
+
+			// module.CheckAndDecide()
 		}
 	}()
 }
 
+func (module Flooding_Module) Proposal(v Proposal) {
+	print("ovo envia")
+	go func() {
+		module.Proposals[module.Round] = Union(module.Proposals[module.Round], []Proposal{v})
 
+		data := make(JSON)
+		data["data"] = module.Proposals[module.Round]
+		data["type"] = Prop
 
-func generateRandom() int{
+		messageJson, _ := json.Marshal(data)
+		encoded := string(messageJson)
+
+		req := BestEffortBroadcast_Req_Message{
+			Addresses: module.Correct_events[1:],
+			Message:   encoded + "§" + module.Correct_events[0]}
+		module.BEB.Req <- req
+	}()
+
+}
+
+func (module Flooding_Module) CheckAndDecide() {
+	if isSubSet(module.Correct_events, module.ReceivedFrom[module.Round]) && !module.Decision {
+		fmt.Print()
+	} else {
+		fmt.Print()
+	}
+}
+
+func (module Flooding_Module) Crash() {
+	//aqui enviar para os outros processos
+}
+
+func Union(a, b []Proposal) []Proposal {
+	m := make(map[Proposal]bool)
+
+	for _, item := range a {
+		m[item] = true
+	}
+
+	for _, item := range b {
+		if _, ok := m[item]; !ok {
+			a = append(a, item)
+		}
+	}
+	return a
+}
+
+func isSubSet(conj1 []string, conj2 []string) bool {
+	return true
+}
+
+func generateRandom() int {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
 	return r1.Intn(100)
 }
 
-
-func encodeJson(proposals []Proposal) string{
+func encodeJson(proposals []Proposal, typeM string) string {
 	messageJson, _ := json.Marshal(proposals)
 
 	return string(messageJson)
 }
 
-func decodeJson(proposals string) []Proposal{
+func decodeJson(proposals string) []Proposal {
 	res := []Proposal{}
 	json.Unmarshal([]byte(proposals), &res)
 
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 // Em caso de erro utilizar o comando GO111MODULE=off no inicio da execução no terminal
 // EX: GO111MODULE=off go run main.go 127.0.0.1:5001  127.0.0.1:6001  127.0.0.1:7001
 
-
 package main
 
 import (
+	"fmt"
 	"os"
+
 	Flooding "./Flooding"
 )
 
@@ -19,11 +20,11 @@ func main() {
 	}
 
 	addresses := os.Args[1:] // lista de ip:port dos outros processos
-	
+
 	module := Flooding.Flooding_Module{}
 
 	module.Init(addresses)
-	
+
 	blq := make(chan int)
 	<-blq
 }
